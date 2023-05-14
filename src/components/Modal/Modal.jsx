880e5dc886294ad296d87dad20bdf43f8c8941b7
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-05-14 22:41:31 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,17 +5,30 @@ export const ModalGame = () => {
 
   const [buttonOffset, setButtonOffset] = useState({ x: 0, y: 0 });
 
-  const handleButtonHover = () => {
-    const randomOffsetX = Math.floor(Math.random() * 210) - 10; // Случайное смещение по оси X от -10 до 10
-    const randomOffsetY = Math.floor(Math.random() * 210) - 10; // Случайное смещение по оси Y от -10 до 10
-
-    setButtonOffset({ x: randomOffsetX, y: randomOffsetY });
+  const handleButtonHover = (event) => {
+    const minOffset = 100; // Минимальное значение смещения (50 пикселей)
+    const randomOffsetX = Math.floor(Math.random() * 21) - 10; // Случайное смещение по оси X от -10 до 10
+    const randomOffsetY = Math.floor(Math.random() * 21) - 10; // Случайное смещение по оси Y от -10 до 10
+  
+    const offsetX = randomOffsetX >= 0 ? randomOffsetX + minOffset : randomOffsetX - minOffset;
+    const offsetY = randomOffsetY >= 0 ? randomOffsetY + minOffset : randomOffsetY - minOffset;
+  
+    const buttonRect = event.target.getBoundingClientRect(); // Получаем размеры и положение кнопки
+    const cursorX = event.clientX; // Координата X курсора мыши
+    const cursorY = event.clientY; // Координата Y курсора мыши
+  
+    // Проверяем, находится ли курсор мыши на том же уровне (по горизонтали) с кнопкой
+    if (cursorY === buttonRect.top || cursorY === buttonRect.bottom) {
+      setButtonOffset({ x: offsetX, y: offsetY });
+    } else {
+      setButtonOffset({ x: 0, y: 0 }); // Сбрасываем смещение, если курсор мыши не находится на том же уровне с кнопкой
+    }
   };
-
+  
     return (
       <Modalwindow>
         <Modalcontent>
-          <ModalHeader>Ты гей?</ModalHeader>
+          <ModalHeader>Володя, ты гей?</ModalHeader>
           <Buttoncontainer>
             <BtnYes>Да</BtnYes>
             <BtnNo             
