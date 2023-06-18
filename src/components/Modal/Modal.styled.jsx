d93e355be1eaaa2dc90f5482c633762c8ff55de2
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-06-18 09:40:16 +0300

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -6,7 +6,7 @@ export const Modalwindow = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    background-image: url("path/to/lgbt-flag.png"); /* Replace with the path or URL of the LGBT flag image */
+    /* background-image: url("path/to/lgbt-flag.png"); Replace with the path or URL of the LGBT flag image */
     background-repeat: no-repeat;
     background-size: cover;
     display: flex;
